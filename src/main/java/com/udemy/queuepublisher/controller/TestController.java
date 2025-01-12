713a2a0faa556fd5041bb52ace9023ae237ef655
david <david_Alisson60@yahoo.com.br>
2025-01-11 22:43:32 -0300

    S.9 - 28 - Rabbit Template With Spring boot

---
+++ src/main/java/com/udemy/queuepublisher/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.udemy.queuepublisher.controller;
+
+
+import com.udemy.queuepublisher.domain.Person;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping(value = "/test/{name}")
+    public String testPI(@PathVariable("name") String name){
+        Person p = new Person(1L, name);
+        rabbitTemplate.convertAndSend("Mobile", p);
+        rabbitTemplate.convertAndSend("Direct-Exchange", "mobile", p);
+        rabbitTemplate.convertAndSend("Fanout-Exchange", "", p);
+        rabbitTemplate.convertAndSend("Topic-Exchange", "tv.mobile.ac", p);
+        return "Success";
+    }
+}
