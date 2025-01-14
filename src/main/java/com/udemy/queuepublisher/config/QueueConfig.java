390c38bea524e8baeafe37288f857ddc11f79666
david <david_Alisson60@yahoo.com.br>
2025-01-13 21:54:35 -0300

    S.9 - 29 - Rabbit Listener With Spring Boot

--- src/main/java/com/udemy/queuepublisher/config/QueueConfig.java
+++ src/main/java/com/udemy/queuepublisher/config/QueueConfig.java
@@ -1,8 +1,11 @@
 package com.udemy.queuepublisher.config;
 
 import com.google.gson.Gson;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class QueueConfig {
@@ -10,5 +13,12 @@ public class QueueConfig {
     @Bean
     public Gson getGson(){
         return new Gson();
+    }
+
+    @Bean
+    public SimpleMessageConverter converter() {
+        SimpleMessageConverter converter = new SimpleMessageConverter();
+        converter.setAllowedListPatterns(List.of("com.udemy.queuepublisher.domain.*", "java.util.*"));
+        return converter;
     }
 }
---
+++ src/main/java/com/udemy/queuepublisher/domain/RabbitMQConsumer.java
@@ -0,0 +1,14 @@
+package com.udemy.queuepublisher.domain;
+
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQConsumer {
+
+    @RabbitListener(queues = "Mobile")
+    public void getMessage(Person p){
+        System.out.println(p.getName());
+    }
+}
